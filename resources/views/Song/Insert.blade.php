f22be4e41561fc388174dc402bdc926e2edf441d
ashrafomid2025 <omidmashraf@gmail.com>
2025-12-13 18:37:13 +0430

    changes in adding musics

--- resources/views/Song/Insert.blade.php
+++ resources/views/Song/Insert.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Adding Songs</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
     <form action="{{ URL('songs/insert') }}" method="post" enctype="multipart/form-data">
         @csrf
+        {{-- cross site request forgery --}}
         <input type="text" name="singer" placeholder="enter the singer name"  class="py-2 px-4 border">
         <input type="file" name="song" accept="audio/*">
         <button type="submit">Save</button>
